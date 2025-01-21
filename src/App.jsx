4e5b6ce17068fb2708e23f50bb7c6eaab676af33
Abozar <abozarafzali11@gmail.com>
2025-01-21 11:58:17 +0100

    Git inti + UI elements

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+
+function App() {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
+  console.log(alphabet)
+
+  const generateAlphabetEl = alphabet.map(letter => <div className='character'>{letter}</div>)
+
+  return (
+    <>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+        <div className="info">
+          “Farewell HTML & CSS” 🫡 
+        </div>
+      </header>
+      <main>
+        <section>
+          <div className="language-container">
+            <div className='language'>HTML</div>
+            <div className='language'>CSS</div>
+            <div className='language'>Javasript</div>
+            <div className='language'>React</div>
+            <div className='language'>Typescript</div>
+            <div className='language'>Node.js</div>
+            <div className='language'>Python</div>
+            <div className='language'>Ruby</div>
+            <div className='language'>Assembly</div>
+          </div>
+        </section>
+        <section className='letters-container'>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+        </section>
+        <section className='characters-container'>
+          {generateAlphabetEl}
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
